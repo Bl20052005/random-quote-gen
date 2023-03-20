48c52b22f5de662aeac9f6aa5c21b8d5791c014f
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2023-03-19 22:11:06 -0700

    ggg gggrr

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ class Display extends React.Component {
         quote: data.content,
         author: data.author
       });
-      console.log(data);
     })
     .catch((err) => {
       console.log(err);
@@ -27,6 +26,14 @@ class Display extends React.Component {
 
   newQuote = () => {
     this.componentDidMount();
+    let firstVal = Math.floor(Math.random() * (9000000-1048575) + 1048575).toString(16);
+    if(parseInt(firstVal.substring(0,2), 16) * 0.299 + parseInt(firstVal.substring(2,4), 16) * 0.587 + parseInt(firstVal.substring(4,6), 16) * 0.114 > 140) {
+      document.querySelector(':root').style.setProperty("--main-color", "#432727");
+    } else {
+      document.querySelector(':root').style.setProperty("--main-color", "#bcd8d8");
+
+    }
+    document.getElementById("root").style.backgroundColor = "#" + firstVal;
   }
   
   render() {
@@ -34,9 +41,11 @@ class Display extends React.Component {
       <div id="quote-box">
         <h1 id="text">{this.state.quote}</h1>
         <h2 id="author">{this.state.author}</h2>
-        <button id="new-quote" onClick={this.newQuote}>New Quote</button>
-        <a href={"twitter.com/intent/tweet?text=" + this.state.quote} target="_blank"><span>tweet it</span></a>
-        <button onClick={() => {navigator.clipboard.writeText(this.state.quote)}} target="_blank"><span>copy it</span></button>
+        <div id="links">
+          <button id="new-quote" onClick={this.newQuote}>New Quote</button>
+          <a id="tweet-quote" href={"https://twitter.com/intent/tweet?hashtags=quotes&text=" + "\"" + this.state.quote  + "\"" + " - " + this.state.author} target="_blank"><span>tweet it</span></a>
+          <button id="copy" onClick={() => {navigator.clipboard.writeText(this.state.quote)}} target="_blank"><span>copy it</span></button>
+        </div>
       </div>
     );
   }
