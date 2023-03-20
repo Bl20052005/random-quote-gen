5f7c5c1cd33f1c96cebbea56d6b1a42c479a9fcc
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2023-03-19 22:10:48 -0700

    4ye5rtbv4 ghdsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      author: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        quote: data.content,
+        author: data.author
+      });
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+  newQuote = () => {
+    this.componentDidMount();
+  }
+  
+  render() {
+    return (
+      <div id="quote-box">
+        <h1 id="text">{this.state.quote}</h1>
+        <h2 id="author">{this.state.author}</h2>
+        <button id="new-quote" onClick={this.newQuote}>New Quote</button>
+        <a href={"twitter.com/intent/tweet?text=" + this.state.quote} target="_blank"><span>tweet it</span></a>
+        <button onClick={() => {navigator.clipboard.writeText(this.state.quote)}} target="_blank"><span>copy it</span></button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default function App() {
+  return(
+    <Display />
+  );
+};
